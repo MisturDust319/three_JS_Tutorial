b71a9dff6d7652f057604159ea197d38b9ae710a
Stan Slupecki <jarlofearls@gmail.com>
2017-04-17 17:02:31 -0400

    Add set up code

--- learnThreeJS/learnThreeJS/js/main.js
+++ learnThreeJS/learnThreeJS/js/main.js
@@ -1,1 +1,22 @@
-﻿// Your code here!
+﻿// simple three.js program
+
+//all threejs progs need these three things:
+//  a scene, a camera, and a renderer
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+    75 // field of view
+    window.innerWidth/window.innerHeight, //aspect ratio
+    //use width/height every time
+    0.1, 1000 //min/max clipping plane
+    )
+
+var renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth,
+    window.innerHeight
+); // the args set the sizefor the draw area
+//  for the app
+// a third arg (updateStyle) can be set false
+//  to render app at 1/2 resolution
+
+document.body.appendChild(renderer.domElement);
